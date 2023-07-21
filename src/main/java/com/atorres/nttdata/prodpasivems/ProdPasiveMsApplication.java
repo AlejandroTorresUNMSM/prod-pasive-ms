2054db2476011a12ad5b1a78b420471d4b41745d
AlejandroTorresUNMSM <alephdmc2575@gmail.com>
2023-07-21 00:05:30 -0500

    dev: commit init

---
+++ src/main/java/com/atorres/nttdata/prodpasivems/ProdPasiveMsApplication.java
@@ -0,0 +1,13 @@
+package com.atorres.nttdata.prodpasivems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProdPasiveMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProdPasiveMsApplication.class, args);
+	}
+
+}
